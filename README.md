e6086c28794fa5595e726653ceccecf3f9be45a5
Mr Stanislaus <meso4life18@gmail.com>
2024-05-27 11:12:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Websocket Server
+ Simple Websocket Server
---
+++ index.js
@@ -0,0 +1,64 @@
+const WebSocket = require("ws");
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const axios = require("axios");
+
+const app = express();
+const wss = new WebSocket.Server({ port: process.env.WSPORT });
+
+let subscriptions = {};
+
+wss.on("connection", (ws) => {
+	console.log("Client connected");
+
+	ws.on("message", async (message) => {
+		const data = JSON.parse(Buffer.from(message).toString());
+		subscriptions[data.channel] = ws;
+	});
+
+	ws.on("close", () => {
+		if (ws.channel) {
+			subscriptions[ws.channel] = subscriptions[ws.channel].filter(
+				(client) => client !== ws
+			);
+		}
+
+		console.log("Client disconnected");
+	});
+});
+
+app.use(bodyParser.json());
+
+app.post("/", (req, res) => {
+	const { channel, event, data } = req.body;
+
+	let subscriptionConnection = subscriptions[channel?.name];
+
+	if (subscriptionConnection) {
+		if (subscriptionConnection.readyState === WebSocket.OPEN) {
+			subscriptionConnection.send(
+				JSON.stringify({
+					event: data.event,
+					channel: channel,
+					data: data,
+				})
+			);
+		}
+	}
+
+	res.sendStatus(200);
+});
+
+app.listen(8080, () => {
+	console.log(
+		`HTTP server is running on ${process.env.PROTOCOL}://${process.env.HOST}:${process.env.PORT}`
+	);
+});
+
+console.log(
+	`WebSocket server is running on ${
+		process.env.PROTOCOL === "http" ? "ws" : "wss"
+	}://${process.env.HOST}:${process.env.WSPORT}`
+);

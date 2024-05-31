d22fc168474ca1e5ebbfcfc6a03b885850f690b3
Mr Stanislaus <meso4life18@gmail.com>
2024-05-31 08:10:51 +0100

    Socket server

--- index.js
+++ index.js
@@ -2,25 +2,29 @@ const WebSocket = require("ws");
 const express = require("express");
 const bodyParser = require("body-parser");
 require("dotenv").config();
-
-const axios = require("axios");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 const wss = new WebSocket.Server({ port: process.env.WSPORT });
 
 let subscriptions = {};
 
-wss.on("connection", (ws) => {
-	console.log("Client connected");
+wss.on("connection", (ws, req) => {
+	console.log(`Client connected`);
 
-	ws.on("message", async (message) => {
+	ws.on("message", (message) => {
 		const data = JSON.parse(Buffer.from(message).toString());
-		subscriptions[data.channel] = ws;
+
+		if (!subscriptions[data.channel]) {
+			subscriptions[data.channel] = new Array(1).fill(ws);
+		} else {
+			subscriptions[data?.channel].push(ws);
+		}
 	});
 
 	ws.on("close", () => {
 		if (ws.channel) {
-			subscriptions[ws.channel] = subscriptions[ws.channel].filter(
+			subscriptions[ws?.channel] = subscriptions[ws.channel].filter(
 				(client) => client !== ws
 			);
 		}
@@ -34,15 +38,15 @@ app.use(bodyParser.json());
 app.post("/", (req, res) => {
 	const { channel, data, event } = req.body;
 
-	let subscriptionConnection = subscriptions[channel?.name];
-
-	if (subscriptionConnection) {
-		if (subscriptionConnection.readyState === WebSocket.OPEN) {
+	subscriptions[channel?.name]?.forEach((ws) => {
+		if (ws.readyState === WebSocket.OPEN) {
 			let socket = data["socket"];
 
 			delete data["socket"];
 
-			subscriptionConnection.send(
+			console.log({ channel, data, event });
+
+			ws.send(
 				JSON.stringify({
 					event: event,
 					channel: channel,
@@ -51,7 +55,7 @@ app.post("/", (req, res) => {
 				})
 			);
 		}
-	}
+	});
 
 	res.sendStatus(200);
 });

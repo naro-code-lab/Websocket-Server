4161980736b1323c9e7af296dca6d20bc5f0213a
Mr Stanislaus <31928163+naro-code-lab@users.noreply.github.com>
2025-05-15 21:41:02 +0100

    Revert "Update index.ts"
    This reverts commit 1457bacc360b84650ab54d6a2dd633e1a923f9b6.

--- src/index.ts
+++ src/index.ts
@@ -125,7 +125,6 @@ router.use((req, res, next) => {
 	const allowedIps = ALLOWED_BROADCASTING_SERVER_IPS.split(",");
 	if (
 		ALLOWED_BROADCASTING_SERVER_IPS === "*" ||
-		allowedIps.includes("*") ||
 		allowedIps.includes(clientIp || "")
 	) {
 		return next();
